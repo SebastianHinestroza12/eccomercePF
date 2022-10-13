06f7f3eb0b115f47fd6ea109d049a2779d8fdce9
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-13 13:19:33 -0300

    quitando Landing y Carousel de App

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Landing from "./components/Landing/Landing";
 import Store from "./Pages/Store";
 import { Route } from "react-router-dom";
-import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <>
-      <Route exact path={"/"}>
-        <Landing />
-      </Route>
-      <Route path={"/store"}>
+
+      <Route path={"/"}>
         <Store />
-      </Route>
-      <Route path={"/carousel"}>
-        <Carousel />
       </Route>
     </>
   );
