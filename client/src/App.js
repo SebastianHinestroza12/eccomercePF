9529a9722a0bac89fa71f27b447225e8f3071817
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-13 13:23:40 -0300

    agregado de Footer y Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Store from "./Pages/Store";
 import { Route } from "react-router-dom";
+import NavScrollExample from "./components/NavBar/navBar";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <>
-
+      <Route>
+        <NavScrollExample />
+      </Route>
       <Route path={"/"}>
         <Store />
+      </Route>
+      <Route>
+        <Footer />
       </Route>
     </>
   );
