e6e5a826e7a24fa20c78f5774afafdfb188558f9
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-10-13 14:19:07 -0500

    rutas(api/controllers y roues/index.js):add get/:id para filtrar por id del producto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,12 +4,15 @@ const postProduct = require('./postProduct');
 const postAdmin = require('./postAdmin');
 const getProduct = require('../controllers/getProduct');
 const putProduct = require('../controllers/putProduct');
+const productId = require('../controllers/productId');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/product', getProduct);
 router.use('/product', putProduct);
+router.use('/product', productId);
+
 
 router.use('/postProduct', postProduct);
 router.use('/postAdmin', postAdmin);
