b77d88096d3ff8cf2b5069838aa927503ace22c5
Edye1230 <edye.1230@gmail.com>
2022-10-13 16:21:03 -0500

    Añadiendo get Categories

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,10 +7,15 @@ const putProduct = require('../controllers/putProduct');
 const postProduct = require('./postProduct');
 const postAdmin = require('./postAdmin');
 
+const getCategories = require('./getCategories');
+
 router.use('/product', getProduct);
 router.use('/product', putProduct);
 
 router.use('/postProduct', postProduct);
+router.use('/getCategories', getCategories);
+
+//
 router.use('/postAdmin', postAdmin);
 
 module.exports = router;
