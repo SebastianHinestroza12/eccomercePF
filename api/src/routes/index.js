4a8e76fec42e8cb6ef418677724912e696d63bfc
Edye1230 <edye.1230@gmail.com>
2022-10-13 12:18:38 -0500

    Actualizando y corrigiendo rutas duplicadas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const postProduct = require('./postProduct');
-const postAdmin = require('./postAdmin');
+
 const getProduct = require('../controllers/getProduct');
 const putProduct = require('../controllers/putProduct');
 
 const postProduct = require('./postProduct');
 const postAdmin = require('./postAdmin');
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 router.use('/product', getProduct);
 router.use('/product', putProduct);
