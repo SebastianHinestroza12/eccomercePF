069a6ad1bb947406bef8747798bc056783f0f75b
Roberto Jimenez <robertojimenezg@gmail.com>
2022-10-13 12:51:11 -0500

    JsonCategories

---
+++ api/src/JSON/jsonCategories.js
@@ -0,0 +1,16 @@
+const jsonCategories = [
+  {
+    "idCategory": "c001",
+    "nameCat": "Jersey"
+  },
+  {
+    "idCategory": "c002",
+    "nameCat": "Balón"
+  },
+  {
+    "idCategory": "c003",
+    "nameCat": "Botín"
+  },
+];
+
+module.exports= jsonCategories;
