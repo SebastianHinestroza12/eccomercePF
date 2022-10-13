636924f58390ff446ae0cfef215795fd516b4508
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-13 05:32:00 -0300

    cargando componente carousel

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Landing from "./components/Landing/Landing";
 import Store from "./Pages/Store";
 import { Route } from "react-router-dom";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Route>
       <Route path={"/store"}>
         <Store />
+      </Route>
+      <Route path={"/carousel"}>
+        <Carousel />
       </Route>
     </>
   );
