b67876ff5c2b1ad78f8153b816ef67212728716c
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-14 12:18:31 -0300

    creado componente newProduct

---
+++ client/src/components/NewProduct/NewProduct.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NewProduct() {
+  return (
+    <div>
+      <form>
+        <input>Productos</input>
+      </form>
+    </div>
+  )
+}
+
+export default NewProduct
