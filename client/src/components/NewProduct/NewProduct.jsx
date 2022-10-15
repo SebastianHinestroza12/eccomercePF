ce283e0a526d020bbafbf7b8f43755809811fa95
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-15 00:00:22 -0300

    formulario completo y funcional

--- client/src/components/NewProduct/NewProduct.jsx
+++ client/src/components/NewProduct/NewProduct.jsx
@@ -1,85 +1,204 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React from "react";
+import { useForm } from "react-hook-form";
+import '../NewProduct/newProduct.css'
 
 function NewProduct() {
-    const {register, formState: {errors}, handleSubmit, watch} = useForm({
-      defaultValues: {
-        nombre: 'Pepita',
-        categoria: 'nada',
-        email: 'christian@hotmial.com'
-      }
-    })
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm({
+    defaultValues: {
+      name: "Balon Adidas",
+      category: "Balones",
+      price: 100,
+      stock: 4,
+      image: "https://dwefffewfweqf.com",
+    },
+  });
 
-    console.log(watch('nombre'))
-    const incluirTelefono = watch('incluirTelefono')
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    const onSubmit = (data) => {
-        console.log(data)
-    }
-
-    const edadValidator = (value) => {
-      return value >= 18 && value <= 65
-    }
+  const selectValidator = (value) => {
+    return value !== "---";
+  };
 
   return (
-    <div>
-        <h1>Nuevo Producto</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <label>Nombre</label>
-            <input type="text" {...register('nombre', {
-                required: true,
-                maxLength: 10,
-                minLength: 5
-            })}/>
-            {errors.nombre?.type === 'required' && <p>El campo nombre es requerido</p>}
-            {errors.nombre?.type === 'maxLength' && <p>Debe tener menos de 10 letras</p>}
-            {errors.nombre?.type === 'minLength' && <p>Debe tener mas de 5   letras</p>}
+    <div class="container">
+      <h1
+        style={{
+          fontFamily: "Verdana",
+          backgroundColor: "#bed6ed",
+          padding: "8px",
+          color: "#292828",
+        }}
+      >
+        Nuevo Producto
+      </h1>
+      <hr />
+      <form class="row g-3 mt-3" onSubmit={handleSubmit(onSubmit)}>
+        <div class="col-md-3">
+          <label htmlFor="name" class="form-label">
+            Nombre
+          </label>
+          <input
+            id="name"
+            class="form-control"
+            type="text"
+            {...register("name", {
+              required: true,
+              pattern: /^[a-zA-Z\s]{0,255}$/,
+              maxLength: 20,
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <p>El campo nombre es requerido</p>
+          )}
+          {errors.name?.type === "maxLength" && (
+            <p>Máximo de carácteres permitidos</p>
+          )}
+          {errors.name?.type === "pattern" && (
+            <p>No se permiten números o símbolos</p>
+          )}
         </div>
-        <div>
-            <label>Categoria</label>
-            <input type="text" {...register('categoria', {
-                required: true,
-                min:1,
-                max:10
-            })}/>
+        <div class="col-md-3">
+          <label htmlFor="category" class="form-label">
+            Categoría
+          </label>
+          <input
+            class="form-control"
+            type="text"
+            id="category"
+            {...register("category", {
+              required: true,
+              pattern: /^[a-zA-Z\s]{0,255}$/,
+            })}
+          />
+          {errors.category?.type === "required" && (
+            <p>El campo nombre es requerido</p>
+          )}
+          {errors.category?.type === "pattern" && (
+            <p>No se permiten números o símbolos</p>
+          )}
         </div>
-        <div>
-            <label>Foto</label>
-            <input type="text" {...register('foto', {
-                required: true,
-                maxLength: 10
-            })}/>
+        <div class="col-md-3">
+          <label htmlFor="price" class="form-label">
+            Precio
+          </label>
+          <input
+            class="form-control"
+            type="text"
+            id="price"
+            {...register("price", {
+              required: true,
+              pattern: /^-?\d*(\.\d+)?$/,
+            })}
+          />
+          {errors.price?.type === "pattern" && <p>Sólo números permitidos</p>}
         </div>
-        <div>
-            <label>Email</label>
-            <input type="text" {...register('email', {
-                pattern: /^\S+@\S+\.\S+$/
-            })}/>
-            {errors.email?.type === 'pattern' && <p>El formato debe ser tipo EMAIL</p>}
+        <div class="col-md-3">
+          <label htmlFor="stock" class="form-label">
+            Stock
+          </label>
+          <input
+            class="form-control"
+            type="text"
+            id="stock"
+            {...register("stock", {
+              required: true,
+              pattern: /^-?\d*(\.\d+)?$/,
+            })}
+          />
+          {errors.stock?.type === "pattern" && <p>Sólo números permitidos</p>}
         </div>
-        <div>
-            <label>Edad</label>
-            <input type="text" {...register('edad', {
-                validate: edadValidator
-            })}/>
-            {errors.edad && <p>La edad debe ser entre 18 y 65 años</p>}
+        <div class="col-md-3">
+          <label htmlFor="image" class="form-label">
+            Imagen
+          </label>
+          <input
+            class="form-control"
+            type="text"
+            id="image"
+            {...register("image", {
+              required: true,
+            })}
+          />
+          {errors.image?.type === "pattern" && (
+            <p>El formato debe ser tipo EMAIL</p>
+          )}
         </div>
-        <div>
-          <label>¿Incluir teléfono?</label>
-          <input type="checkbox" {...register('incluirTelefono')}/>
+        <div class="col-md-3">
+          <label htmlFor="stars" class="form-label">
+            Puntuación
+          </label>
+          <select
+            id="stars"
+            name="stars"
+            class="form-select"
+            aria-label="Default select example"
+            {...register("stars", {
+              validate: selectValidator,
+            })}
+          >
+            <option selected>---</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          {errors.stars && <p>Debes seleccionar una opción</p>}
         </div>
-        {incluirTelefono && (
-        <div>
-          <label>Teléfono</label>
-          <input type="text" {...register('telefono')}/>
+        <div class="col-md-3">
+          <label htmlFor="size" class="form-label">
+            Tamaño
+          </label>
+          <select
+            id="size"
+            name="size"
+            class="form-select"
+            aria-label="Default select example"
+            {...register("size", {
+              validate: selectValidator,
+            })}
+          >
+            <option selected>---</option>
+            <option value="xs">XS</option>
+            <option value="s">S</option>
+            <option value="m">M</option>
+            <option value="l">L</option>
+            <option value="xl">XL</option>
+            <option value="xxl">XXL</option>
+          </select>
+          {errors.size && <p>Debes seleccionar una opción</p>}
         </div>
-
-        )}
-        <input type="submit" value="Enviar" />
+        <div class="col-md-6">
+          <div class="form-floating">
+            <textarea
+              class="form-control"
+              placeholder="Leave a comment here"
+              id="floatingTextarea"
+              {...register("detail", {
+                required: true
+              })}
+            ></textarea>
+            <label for="floatingTextarea">Detalles</label>
+            {errors.detail?.type === "required" && (
+            <p>El campo detalles es requerido</p>
+          )}
+          </div>
+        </div>
+        <div class="col-12 mt-5">
+          <button type="submit" class="btn btn-danger">
+            Enviar
+          </button>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default NewProduct
+export default NewProduct;
