106a59c901e98931211c94a5c602e425afd54dba
Roberto Jimenez <robertojimenezg@gmail.com>
2022-10-18 08:47:36 -0500

    prueba de errores

--- api/src/db.js
+++ api/src/db.js
@@ -54,3 +54,4 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+/*Comentario*/
