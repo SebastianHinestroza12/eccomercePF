39457b9feb11e69a12797034b1e46bfd54db65af
Lissette Lindao <lnlindao@gmail.com>
2022-10-21 13:34:56 -0500

    Production configurations

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,15 @@ import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 import { Auth0Provider } from "@auth0/auth0-react";
+import dotenv from "dotenv";
+require("dotenv").config();
 
-axios.defaults.baseURL = "http://localhost:3001";
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+//axios.defaults.baseURL =  "https://api-production-e274.up.railway.app/" || "http://localhost:3001";
+//const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 // console.log(process.env.REACT_APP_AUTH0_DOMAIN)
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
