150ed3103e514c577fb87e78bdff26e081d9e887
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-25 12:31:55 -0300

    agregado de paginado sin funcionalidad

--- client/src/components/PanelAdmin/PanelAdmin.jsx
+++ client/src/components/PanelAdmin/PanelAdmin.jsx
@@ -12,23 +12,51 @@ function PanelAdmin() {
 
   return (
     <Fragment>
-      {/* --------------BOTONES---------------- */}
+      <br/>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <button onClick={getProducts}>Cargar productos</button>
       <div class="table-responsive">
-      <table class="table" style={{ width: "100%" }}>
-        <thead>
-          <tr>
-            <th style={{ width: "330px" }}>#</th>
-            <th style={{ width: "350px" }}>Nombre</th>
-            <th style={{ width: "100px" }}>Precio</th>
-            <th style={{ width: "100px" }}>Visible</th>
-            <th style={{ width: "350px" }}>Detalle</th>
-            <th style={{ width: "100px" }}>checkbox</th>
-            <th style={{ width: "80px" }}>boton1</th>
-            <th style={{ width: "80px" }}>boton2</th>
-          </tr>
-        </thead>
-      </table>
+        <table class="table" style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th style={{ width: "330px" }}>#</th>
+              <th style={{ width: "350px" }}>Nombre</th>
+              <th style={{ width: "100px" }}>Precio</th>
+              <th style={{ width: "100px" }}>Visible</th>
+              <th style={{ width: "350px" }}>Detalle</th>
+              <th style={{ width: "100px" }}>checkbox</th>
+              <th style={{ width: "80px" }}>boton1</th>
+              <th style={{ width: "80px" }}>boton2</th>
+            </tr>
+          </thead>
+        </table>
       </div>
       {products.map((e) => (
         <div class="table-responsive">
@@ -41,7 +69,9 @@ function PanelAdmin() {
                 <td style={{ width: "100px", fontSize: "small" }}>
                   {e.visible.toString()}
                 </td>
-                <td style={{ width: "350px", fontSize: "small" }}>{e.detail}</td>
+                <td style={{ width: "350px", fontSize: "small" }}>
+                  {e.detail}
+                </td>
                 <td style={{ width: "100px", fontSize: "small" }}>
                   <label htmlFor="agotado">Agotado&nbsp;</label>
                   <input id="agotado" type="checkbox" />
