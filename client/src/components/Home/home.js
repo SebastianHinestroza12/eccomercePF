bc501af4ce4ebe3ad6a9abc5f08b92cbff2373dd
ivokoby <ivokoby@gmail.com>
2022-10-27 23:30:25 -0300

    reviews

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -101,14 +101,41 @@ const Home = () => {
               VER MÁS <Unicons.UilArrowRight />
             </Link>
           </Col>
-          <Col></Col>
         </div>
       </section>
 
-      <div className="testimonios">
-        <h2>Testimonios</h2>
-        <p>proximamente</p>
-      </div>
+      <h2 className="testimonios">Testimonios</h2>
+
+      <section className="testimonios">
+        <div className="testimonios-contenedor">
+          {allProducts.slice(0, 6).map((product) => {
+            return (
+              <Col md={2} xs={4} key={product.id}>
+                <div className="testimonios-caja">
+                  <div className="caja-top">
+                    <div className="perfil">
+                      <div className="perfil-img">
+                        <img src="" alt=""/>
+                      </div>
+                      <div className="name-user">
+                        <strong>User name</strong>
+                        <span>{product.name}</span>
+                      </div>
+                    </div>
+                    <div className="reviews">
+                      <i className="fas fa-star">★★★★★</i>
+                    </div>
+                    </div>
+                    <div className="comentarios">
+                      <p>"Me encanto este producto, me llego 
+                      en buen estado y no tardo en llegar. Lo recomiendo!!"</p>
+                    </div>
+                </div>
+              </Col>
+            );
+          })}
+        </div>
+      </section>
     </Container>
   );
 };
