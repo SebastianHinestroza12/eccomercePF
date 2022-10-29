b851372a8e8a6c2e6c5720868f5a7cc84fcdb00b
ChristianSpeziali <spezialichristian@gmail.com>
2022-10-29 18:44:13 -0300

    middlewares revisados y entendidos

--- api/src/controllers/User/getPrueba.js
+++ api/src/controllers/User/getPrueba.js
@@ -4,29 +4,35 @@ const jwks = require("jwks-rsa");
 const axios = require("axios");
 const jwtAuthz = require("express-jwt-authz");
 
+// const verifyJwt = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: "https://qatar-e-shop.us.auth0.com/.well-known/jwks.json",
+//   }),
+//   audience: "This is a unique identifier",
+//   issuer: "https://qatar-e-shop.us.auth0.com/",
+//   algorithms: ["RS256"],
+// }).unless({ path: ["/user/prueba"] });
 const verifyJwt = jwt({
   secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: "https://qatar-e-shop.us.auth0.com/.well-known/jwks.json",
+    jwksUri: "https://qatar-e-shop.us.auth0.com/.well-known/jwks.json", // aqui comprueba que se este loguado en la web de qatar y no en cualquier otro lado (obligado)
   }),
-  audience: "This is a unique identifier",
-  issuer: "https://qatar-e-shop.us.auth0.com/",
-  algorithms: ["RS256"],
-}).unless({ path: ["/user/prueba"] });
-
-const checkPermissions = jwtAuthz(["read:users"], {
-
-    customScopeKey: "permissions",
-    customUserKey: "auth",
-    // checkAllScopes: true
-    // debe cumplir todos los permisos especificados
+  
+  algorithms: ["RS256"], //necesita si o si que se le indique el tipo de algoritmo (obligado)
 })
 
-router.get('/role', verifyJwt, checkPermissions,(req, res)=>{
+const checkPermissions = jwtAuthz(["read:users", "write:users"], {
+  customScopeKey: "permissions",
+  customUserKey: "auth",
+  checkAllScopes: true
+  // debe cumplir todos los permisos especificados
+});
+
+router.get("/role", verifyJwt, checkPermissions, (req, res) => {
   res.send("Accediendo a ruta ROLES");
-})
+});
 
 router.get("/", (req, res) => {
   try {
@@ -43,6 +49,5 @@ router.get("/protected", verifyJwt, (req, res) => {
     console.log(error.message);
   }
 });
-
 
 module.exports = router;
