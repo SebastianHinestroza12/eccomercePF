490e808cc03a1c3b7800a9506ac39272dd38c4c5
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-10-31 12:07:01 -0500

    admin resolver(client/componets/admin)

--- client/src/components/Admin/LayoutAdmin.js
+++ client/src/components/Admin/LayoutAdmin.js
@@ -1,58 +1,39 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Col, Container, Row } from "react-bootstrap";
 import AdminContent from "./AdminContent";
 import AdminHeader from "./AdminHeader";
 import AdminSidebar from "./AdminSidebar";
 import "./admin.css";
-// import { withAuthenticationRequired } from "@auth0/auth0-react";
-import { UilLockSlash } from "@iconscout/react-unicons";
+
 
 const LayoutAdmin = () => {
-  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
-  const [aprobado, setAprobado] = useState(true);
-  useEffect(() => {
-    const passToken = async () => {
-      const token = await getAccessTokenSilently();
-      console.log(token);
-      await axios
-        .get("/user/prueba/role", {
-          headers: { authorization: `Bearer ${token}` },
-        })
-        .catch((e) => {
-          console.log(e.response);
-          setAprobado(false);
-        });
-    };
-    passToken();
-  }, []);
+  const { user, isAuthenticated } = useAuth0();
+  const name = () => {
+    if (user.email === "qatareshop08@gmail.com") return true
+    else return false
+  }
   return (
-    <>
-      {aprobado === false ? (
-        <div class="text-bg-danger p-5">
-          <UilLockSlash />
-          <span class="ms-5">
-            NO TIENES LOS PERMISOS NECESARIOS PARA ACCEDER
-          </span>
-        </div>)
-        : (
-          <Container fluid className="admin">
-            <Row>
-              <Col md={2} className="sidebar">
-                <AdminSidebar />
-              </Col>
-              <Col>
-                <AdminHeader />
-                <div className="body flex-grow-1 px-3">
-                  <AdminContent />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        )}
-    </>
+    <div>
+      {
+        isAuthenticated && name() ?
+          (
+            <Container fluid className="admin">
+              <Row>
+                <Col md={2} className="sidebar">
+                  <AdminSidebar />
+                </Col>
+                <Col>
+                  <AdminHeader />
+                  <div className="body flex-grow-1 px-3">
+                    <AdminContent />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          ) : <div>No tenes permiso para ingresar</div>
+      }
+    </div>
   );
 };
 export default LayoutAdmin;
-// export default LayoutAdmin;
+// export default La/* Exportando el componente. */
