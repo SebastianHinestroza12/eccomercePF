7752fe3a7862e35663fae5bae58f8f79cfaae685
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-11-02 21:43:28 -0500

    merge ramaFinal2

--- api/index.js
+++ api/index.js
@@ -24,17 +24,17 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     server.listen(port, () => {
       console.log("%s listening at 3001"); // eslint-disable-line no-console
     });
   })
   .then(() => {
-    Category.findOrCreate({ where: {name: "Jersey"} });
-    Category.findOrCreate({ where: {name: "Balon"} });
-    Category.findOrCreate({ where: {name: "Calzado"} });
-    Category.findOrCreate({ where: {name: "Short"} });
+    Category.findOrCreate({ where: { name: "Jersey" } });
+    Category.findOrCreate({ where: { name: "Balon" } });
+    Category.findOrCreate({ where: { name: "Calzado" } });
+    Category.findOrCreate({ where: { name: "Short" } });
 
     console.log("Categorias creadas");
   });
