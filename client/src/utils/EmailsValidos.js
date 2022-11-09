89d6ee927f2b6f1035a3c8024535b79b24125ad4
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-11-09 15:33:02 -0500

    gmail roberto

--- client/src/utils/EmailsValidos.js
+++ client/src/utils/EmailsValidos.js
@@ -4,6 +4,7 @@ export const listEmails = [
   "lnlindao@gmail.com",
   "edye.1230@gmail.com",
   "menas7527@gmail.com",
+  "robertojimenezg@gmail.com",
   "ivokoby@gmail.com",
   "gregorioescobara28@gmail.com",
   "exequielcostabel@gmail.com",
