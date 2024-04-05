0e86f9731422fa45bf78c1ada980702e6e5618ca
Sebastian Mena <99746056+SebastianHinestroza12@users.noreply.github.com>
2024-04-05 05:29:18 -0500

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/eccomerce`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/eccomercedb_gihr`,
         {
           logging: false, // set to console.log to see the raw SQL queries
           native: false, // lets Sequelize know we can use pg-native for ~30% more speed
