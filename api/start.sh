9cdd1afb80bc2ee817fa2644f5d286f5eab0b96d
Sebastian Mena <99746056+SebastianHinestroza12@users.noreply.github.com>
2024-04-06 09:04:16 -0500

    Changes

--- api/start.sh
+++
@@ -1,4 +0,0 @@
-# Agregar el directorio de binarios de Poetry a la variable de entorno PATH
-curl -sSL https://install.python-poetry.org | python3
-
-export PATH="/opt/render/project/poetry/bin:$PATH"
